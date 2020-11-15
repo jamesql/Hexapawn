3d884d4880e2899e97b3852444c6b6e5f2d55f56
= <thejamesash1@gmail.com>
2020-11-15 10:12:41 -0500

    start asset header

--- Assets/assets.h
+++ Assets/assets.h
@@ -4,12 +4,33 @@ namespace Assets
 
 	class Board
 	{
-	public: 
-		void test();
+	public:
+		// Vars
+		Piece bpcs[9];	
+
+		char* move(Piece op, Piece np);
+		bool checkMove(Piece p);
+		char* createBoard();
+		void addPiece(Piece p);
+		void delPiece(Piece p);
+		char* getBoard(); 
+		void printBoard();
+
 	};
 
 	class Menu
 	{
 	
 	};
+
+	class Piece
+	{
+	
+	};
+
+	class Move
+	{
+
+	};
+
 }
