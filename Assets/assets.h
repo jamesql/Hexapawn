852c15993107f60411973f9de3b5ac760c1cab83
= <thejamesash1@gmail.com>
2020-11-15 10:18:23 -0500

    start piece header

--- Assets/assets.h
+++ Assets/assets.h
@@ -7,14 +7,22 @@ namespace Assets
 	public:
 		// Vars
 		Piece bpcs[9];	
-
+		Team white;
+		Team black;
+	
 		char* move(Piece op, Piece np);
 		bool checkMove(Piece p);
+		Move[] getMoves(Team t);
 		char* createBoard();
 		void addPiece(Piece p);
 		void delPiece(Piece p);
 		char* getBoard(); 
 		void printBoard();
+
+	};
+
+	class Team
+	{
 
 	};
 
@@ -25,7 +33,11 @@ namespace Assets
 
 	class Piece
 	{
-	
+	public: 
+		// Vars
+		Team tm;
+
+		Piece(Team t);
 	};
 
 	class Move
