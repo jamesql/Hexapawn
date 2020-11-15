0f54e5a052829c4afc639a168f6620cead64f357
= <thejamesash1@gmail.com>
2020-11-15 13:25:43 -0500

    add piece.cpp with functions

---
+++ Assets/piece.cpp
@@ -0,0 +1,25 @@
+// Piece Asset
+
+#include "assets.h"
+
+namespace Assets 
+{
+
+	Move* Piece::getMoves()
+	{
+		return null;
+	}
+
+	Move Piece::movePiece(int newX, int newY)
+	{
+		return null;
+	}
+
+	Piece::Piece(Team t, int x, int y)
+	{
+		Piece::tm = t;
+		Piece::posX = x;
+		Piece::posY = y;
+	}
+
+}
